13deda54e2ba9f23a6152cd96c4705be6b7dfe6f
IpePiji <hafizhi.z@okwave.global>
2020-10-21 22:37:49 +0800

    Add full-calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DataTablesModule } from 'angular-datatables';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin,
+  bootstrapPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -18,7 +28,8 @@ import { DataTablesModule } from 'angular-datatables';
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    DataTablesModule
+    DataTablesModule,
+    FullCalendarModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/customer/dashboard/dashboard.component.ts
+++ src/app/customer/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +12,28 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    weekends: true,
+    eventClick: this.handleEventClick.bind(this),
+    events: [
+      { title: 'Repair', date: '2020-10-27', display: 'background' },
+      { title: 'Checking', date: '2020-10-30', display: 'background' }
+    ],
+    height: "100%",
+    buttonText: {
+      today:    'Today',
+    },
+    themeSystem: 'bootstrap'
+  };
+
+  handleEventClick(arg): void {
+    Swal.fire({
+      text: arg.event.title,
+      icon: 'success'
+    });
   }
 
 }
